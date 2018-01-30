64203651fcc6edf94543361bb0840e1847abb6d4
Cristopher Caceres <cristopher.cc92@gmail.com>
2018-01-29 20:23:41 -0400

    add home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Grid from 'material-ui/Grid';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>El Baratón</h1>
-      </div>
+      <Grid item xs={12}>
+        <Grid item xs={12}>
+          <h1>El Baratón</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <img className="home-image" src="/images/vino_0.jpg"></img>
+        </Grid>
+      </Grid>
     );
   }
 }
