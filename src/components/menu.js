e7d1bb4aad5f1f65c4a9d198587a159dfe61f37b
Cristopher Caceres <cristopher.cc92@gmail.com>
2018-01-30 22:15:00 -0400

    FEAT menu with categories and sublevels

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,6 +3,10 @@ import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import { getCategories } from '../actions/categories';
 import { connect } from 'react-redux';
+import Collapse from 'material-ui/transitions/Collapse';
+import ExpandLess from 'material-ui-icons/ExpandLess';
+import ExpandMore from 'material-ui-icons/ExpandMore';
+import TextField from 'material-ui/TextField';
 
 class MenuList extends Component {
   constructor(props) {
@@ -10,12 +14,23 @@ class MenuList extends Component {
     this.state = {
       isLoad: false,
       categories: [],
+      openCategories: []
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentWillReceiveProps( nextProps ) {
     if (!this.state.isLoad) {
       if (nextProps.categories.length > 0) {
+
+        for (let i = 0; i < nextProps.categories.length; i++) {
+          const openCategories = this.state.openCategories;
+          openCategories.push(false);
+          this.setState({
+            openCategories: openCategories,
+          })
+        }
+
         this.setState({
           isLoad: true,
           categories: nextProps.categories
@@ -27,8 +42,48 @@ class MenuList extends Component {
   componentDidMount() {
     this.props.getCategories();
   }
-  
+
+  handleClick(index) {
+    const openCategories = this.state.openCategories;
+    if ( openCategories[index] ) {
+      openCategories[index] = false;
+    } else {
+      openCategories[index] = true;
+    }
+    this.setState({
+      openCategories: openCategories
+    });
+  }
+
   render() {
+
+    const Menu = ({ data, level, id }) => {
+
+      level = level + 1;
+      const padding = 16 * level;
+      const paddingText = padding+'px';
+      const openSublevels = this.state.openCategories;
+
+      return (
+        <div>
+        {data.map(sublevel => {
+            return (
+              <div>
+                <ListItem button style={{'paddingLeft': paddingText}}>
+                  <ListItemText primary={sublevel.name} />
+                </ListItem>
+                {sublevel.sublevels ?
+                  ( <Menu data={sublevel.sublevels} level={level}/> )
+                  :
+                  ( <div>  </div> )
+                }
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div>
         <List>
@@ -42,9 +97,15 @@ class MenuList extends Component {
         <Divider />
         { this.state.categories.map( (row, index ) => {
             return (
-              <ListItem button>
-                <ListItemText primary={ row.name } />
-              </ListItem>
+              <div>
+                <ListItem button onClick={() => this.handleClick(index)}>
+                  <ListItemText primary={ row.name } />
+                  {this.state.openCategories[index] ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={ this.state.openCategories[index] } timeout="auto" unmountOnExit>
+                  <Menu data={ row.sublevels } level={ 1 } index={ row.id }/>
+                </Collapse>
+              </div>
             );
           }
         )}
